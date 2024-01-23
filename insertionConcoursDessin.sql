71d9e1db023ba8349e914ac160c3a32244163f0a
julessalaun <157136176+julessalaun@users.noreply.github.com>
2024-01-23 11:26:04 +0100

    Delete insertionConcoursDessin.sql

--- insertionConcoursDessin.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Club (nomClub, adresse, numTelephone, nombreAdherents, ville, departement, region) VALUES
-('Club  Paris', '123 Rue de Paris', '0123456789', 20, 'Paris', 'Paris', 'Île-de-France'),
-('Club  Paris', '456 Rue de Paris', '0123456798', 20, 'Paris', 'Paris', 'Île-de-France'),
-('Club de  Versailles', '789 Rue de Versailles', '0234567890', 20, 'Versailles', 'Yvelines', 'Île-de-France'),
-('Club de  St-Germain', '321 Rue de St-Germain', '0234567891', 20, 'Saint-Germain-en-Laye', 'Yvelines', 'Île-de-France'),
-('Club de  Lyon', '654 Rue de Lyon', '0345678902', 20, 'Lyon', 'Rhône', 'Auvergne-Rhône-Alpes'),
-('Club de  Lyon', '987 Rue de Lyon', '0345678903', 20, 'Lyon', 'Rhône', 'Auvergne-Rhône-Alpes'),
-('Club de Golf Grenoble', '147 Rue de Grenoble', '0456789024', 20, 'Grenoble', 'Isère', 'Auvergne-Rhône-Alpes'),
-('Club de Grenoble', '258 Rue de Grenoble', '0456789025', 20, 'Grenoble', 'Isère', 'Auvergne-Rhône-Alpes');
