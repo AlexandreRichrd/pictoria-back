6c1ac71bcfec68f7288a8f20702df925eb597213
julessalaun <157136176+julessalaun@users.noreply.github.com>
2024-01-24 16:30:35 +0100

    Update insertion.sql

--- insertion.sql
+++ insertion.sql
@@ -446,7 +446,7 @@ VALUES
 
 
 -- Insertion pour la table Dessin
-INSERT INTO Dessin (numCompetiteur, commentaire, classement, dateRemise, leDessin)
+INSERT INTO Dessin (numCompetiteur, commentaire, dateRemise, leDessin)
 VALUES
     (1, 'Commentaire1', 1, '2024-02-05', 'Chemin/vers/leDessin1.png'),
     (2, 'Commentaire2', 2, '2024-02-10', 'Chemin/vers/leDessin2.png');
